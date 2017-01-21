7ba49a576bd327c55a0598c99bd30f15a67e8e18
Adrian Staniec <adrianstaniec@gmail.com>
2017-01-21 12:55:11 +0100

    Frist working version

---
+++ codemetrics.py
@@ -0,0 +1,34 @@
+"""
+CodeMetrics - python script summarizing source code base
+"""
+
+import os           # for file access
+import sys          # for command line arguments
+import pandas as pd # for command line arguments
+
+def scan_file(arg):
+    dummy, ext = os.path.splitext(arg)
+    return ext
+
+def main():
+    if len(sys.argv) == 2:
+        origin = sys.argv[1]
+    else:
+        origin = os.path.abspath('.')
+
+    ext_index = pd.Series(dtype = 'int64')
+
+    for dummy, dummy, filenames in os.walk(origin):
+        for filename in filenames:
+            ext = scan_file(filename)
+            if ext_index.get(ext) == None:
+                ext_index[ext] = 1
+            else:
+                ext_index[ext] += 1
+
+    ext_index = ext_index.sort_values(ascending=False)
+
+    print("File count results:")
+    print(ext_index)
+
+main()
