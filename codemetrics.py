bc0e44e4c5aa3f6f06c2831751094b90aee8c6ca
Adrian Staniec <adrianstaniec@gmail.com>
2017-01-29 00:29:00 +0100

    add histobars; split files and lines

--- codemetrics.py
+++ codemetrics.py
@@ -2,9 +2,18 @@
 CodeMetrics - python script summarizing source code base
 """
 
+import math         # for flooring floats
 import os           # for file access
 import sys          # for command line arguments
 import pandas as pd # for data storage and manipulation
+
+def determine_source_dir(args):
+    """Determine source code directory path"""
+    if len(args) == 2:
+        origin = args[1]
+    else:
+        origin = os.path.abspath('.')
+    return origin
 
 def scan_file(filepath):
     """Analyzes a single file's name and content"""
@@ -29,14 +38,8 @@ def scan_file(filepath):
 
     return ext, lncnt, omitted
 
-def main():
-    """Main function"""
-    # Process command line arguments
-    if len(sys.argv) == 2:
-        origin = sys.argv[1]
-    else:
-        origin = os.path.abspath('.')
-
+def gather_info(origin):
+    """Crawl the directory for stats"""
     # Prepare data structures
     file_types = pd.Series(dtype='int64', name='Files')
     code_lines = pd.Series(dtype='int64', name='Lines')
@@ -54,18 +57,53 @@ def main():
             except KeyError:
                 summary.loc[ext] = 1, lncnt
 
-    # Format data
-    summary = summary.sort_values('Lines', ascending=False)
-    summary = summary.sort_values('Files', ascending=False)
+    return summary, omit_cnt
 
-    # Print results
-    print("")
-    print("Summary:")
-    print(summary.head(20))
+
+def calc_scaling_factor(ser):
+    """Calculate the facotr for scaling data to charbar"""
+    line_width = os.get_terminal_size().columns
+    avail_cols = line_width - 20
+    max_val = ser.max()
+    factor = 1
+    if ser.max() > avail_cols:
+        factor = avail_cols / max_val
+    return factor
+
+
+def print_column(ser, factor):
+    """Print a single column"""
+    ser = ser.sort_values(ascending=False)
+    cnt = 0
+    for ind, val in ser.iteritems():
+        print(str(ind).ljust(6), str(val).rjust(7), end='')
+        print(' ', '*' * math.floor(val * factor))
+        cnt += 1
+        if cnt >= 10:
+            print('...')
+            break
+
+
+def print_summary(data, omit_cnt):
+    """Print summary text to output"""
+    print("\nFile types:")
+    factor = calc_scaling_factor(data['Files'])
+    print_column(data['Files'], factor)
+
+    print("\nCode lines:")
+    factor = calc_scaling_factor(data['Lines'])
+    print_column(data['Lines'], factor)
 
     if omit_cnt > 0:
         print("")
         print("Lines in {0} files were not counted, due to decoding problems.".format(omit_cnt))
+
+
+def main():
+    """Main function"""
+    origin = determine_source_dir(sys.argv)
+    summary, omit_cnt = gather_info(origin)
+    print_summary(summary, omit_cnt)
 
 if __name__ == "__main__":
     main()
