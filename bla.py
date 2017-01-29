59c303ed72d9854288bdafff431259ea769c1439
Adrian Staniec <adrianstaniec@gmail.com>
2017-01-29 13:52:45 +0100

    untrack .vscode dir

--- bla.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QFileDialog
-
-app = QApplication(sys.argv)
-qpath = QFileDialog.getExistingDirectory(None,
-"KodeMetrics: Select the project source code directory")
-print(os.path.abspath(qpath))
